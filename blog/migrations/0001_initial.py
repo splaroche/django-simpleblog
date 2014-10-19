8e2c63fa699735a4ab772992e2ee781feb0f7401
splaroche <stevenplaroche@gmail.com>
2014-10-19 17:02:02 -0600

    Adds the author field.  Adds code to use ckeditor when django-ckeditor is installed.

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -2,18 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
+
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(unique=True, max_length=100)),
                 ('slug', models.SlugField(unique=True, max_length=100)),
             ],
@@ -24,14 +26,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Entry',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(unique=True, max_length=100)),
                 ('slug', models.SlugField(unique=True, max_length=100)),
                 ('body', models.TextField()),
-                ('posted',
-                 models.DateTimeField(auto_now_add=True, db_index=True)),
+                ('posted', models.DateTimeField(auto_now_add=True, db_index=True)),
                 ('published', models.BooleanField(default=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
                 ('category', models.ManyToManyField(to='blog.Category')),
             ],
             options={
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import permalink
+
+try:
+    from ckeditor.fields import RichTextField as TextField
+except ImportError:
+    from django.db.models import TextField
+
 
 
 class Category(models.Model):
@@ -17,10 +24,11 @@ class Category(models.Model):
 class Entry(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    body = models.TextField()
+    body = TextField()
     posted = models.DateTimeField(db_index=True, auto_now_add=True)
     published = models.BooleanField(default=True)
     category = models.ManyToManyField(Category)
+    author = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.title
